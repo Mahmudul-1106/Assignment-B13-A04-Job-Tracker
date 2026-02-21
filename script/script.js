d782287632a2061a38b7454111121f97a7918378
mahmudul <mahmudul1106@gmail.com>
2026-02-22 04:21:38 +0600

    delet working

--- script/script.js
+++ script/script.js
@@ -1,12 +1,55 @@
 console.log('Js added.')
 // Total Job Count
+function totalJobCount(){
+    const totalJob = document.getElementById('total-job')
+    const jobContainer = document.getElementById('job-card-container');
+    const jobCount = jobContainer.getElementsByClassName('job-card');
+    totalJob.innerText = jobCount.length;  
+}
 
-const totalJob = document.getElementById('total-job')
-const jobContainer = document.getElementById('job-card-container');
-const jobCount = jobContainer.getElementsByClassName('job-card');
-totalJob.innerText = jobCount.length;
+
+// clicking All Button function
+document.getElementById('btn-all').addEventListener('click', function(){
+
+   
+    document.getElementById('job-card-container').classList.remove('hidden');
+    document.getElementById('Interview-section').classList.add('hidden');
+
+    document.getElementById('btn-all').classList.remove('btn-soft');
+    document.getElementById('btn-interview').classList.add('btn-soft');
+})
 
 
+
+// Clicking Interview button Functions
+document.getElementById('btn-interview').addEventListener('click', function(){
+
+    const interviewJobs = document.getElementById('interview-job').innerText;
+    if(Number(interviewJobs) > 0){
+        document.getElementById('interview-zero').classList.add('hidden');
+    }
+    if(Number(interviewJobs) == 0){
+        document.getElementById('interview-zero').classList.remove('hidden');
+    }
+
+
+    document.getElementById('job-card-container').classList.add('hidden');
+    document.getElementById('Interview-section').classList.remove('hidden');
+
+    document.getElementById('btn-all').classList.add('btn-soft');
+    document.getElementById('btn-interview').classList.remove('btn-soft');
+})
+
+
+// clicking delete button functions
+function Delete(id){
+   const deleteJob = document.getElementById(id).remove();
+   
+   totalJobCount();
+}
+
+
+// Interview function
 function Interview(id){
     console.log('Button clicked')
     const jobCard = document.getElementById(id);
@@ -26,3 +69,5 @@ function Interview(id){
 
 }
 
+
+totalJobCount();
